a95b2fc52769f81c12b785546cd302d658dd5dd2
zenith-noble01 <webdev686@gmail.com>
2021-07-20 15:36:33 +0100

    form validator

--- script.js
+++ script.js
@@ -1,85 +1,97 @@
-
+       //validation of firstname
     $('#firstname').keyup(function(){
         if(validationOfFirstName()){
             $('#firsname_error').text('Username validated');
-            $('#firsname_error').css('color', 'blue');
-            $('.input1').css('border-bottom', '1px solid blue')
+            $('#firsname_error').css('color', '#00ff00');
+            $('.input1').css('border-bottom', '1px solid #00ff00')
         }else{
             $('#firsname_error').text('Please use a valid Name');
-            $('#firsname_error').css('color', 'red');
-                $('.input1').css('border-bottom', '1px solid red')
+            $('#firsname_error').css('color', '#ff0000');
+                $('.input1').css('border-bottom', '1px solid #ff0000')
         }
     })
+
+    //validation of secondname
     $('#lastname').keyup(function(){
         if(validationOfLastName()){
             $('#wrong_name').text('Username validated');
-            $('#wrong_name').css('color', 'blue');
-            $('.input2').css('border-bottom', '1px solid blue')
-            // $('.input1').css('border-bottom', '1px solid blue')
+            $('#wrong_name').css('color', '#00ff00');
+            $('.input2').css('border-bottom', '1px solid #00ff00')
+            // $('.input1').css('border-bottom', '1px solid #00ff00')
         }else{
             $('#wrong_name').text('Please use a valid Name');
-            $('#wrong_name').css('color', 'red');
-                $('.input2').css('border-bottom', '1px solid red')
+            $('#wrong_name').css('color', '#ff0000');
+                $('.input2').css('border-bottom', '1px solid #ff0000')
         }
     })
 
-    
 
-    $('#second').keyup(function(){
-        if($('#first').val() === $('#second').val()){
-            $('#confirm_password_error').text('we are ready to go')
-            $('#confirm_password_error').css('color', 'blue')
-            $('.input6').css('border-bottom', '1px solid blue')
-        }
-        else{
-            $('#confirm_password_error').text('your passwords do not match please check them again')
-            $('#confirm_password_error').css('color', 'red')
-              $('.input6').css('border-bottom', '1px solid red')
+          //validation if email
+      $('#email').keyup(function(){
+        if(ValidationOfEmail()){
+            $('#email_error').text('Email Validated')
+            $('#email_error').css('color', '#00ff00');
+            $('.input3').css('border-bottom', '1px solid #00ff00')
+        } else{
+            $('#email_error').text('Please use a valid Email');
+            $('#email_error').css('color', '#ff0000');
+            $('.input3').css('border-bottom', '1px solid #ff0000')
         }
     })
 
+          //validation of phonenumber
     $('#phone').keyup(function(){
-
         if(validationOfPhone()){
-            $('#phone_error').text('we are not ready to go')
-            $('#phone_error').css('color', 'blue')
-            $('.input4').css('border-bottom', '1px solid blue')
+            $('#phone_error').text('Phone number validated')
+            $('#phone_error').css('color', '#00ff00')
+            $('.input4').css('border-bottom', '1px solid #00ff00')
     }else{
         $('#phone_error').text('please enter a valid phone number')
-        $('#phone_error').css('color', 'red')
-        $('.input4').css('border-bottom', '1px solid red')
+        $('#phone_error').css('color', '#ff0000')
+        $('.input4').css('border-bottom', '1px solid #ff0000')
     }
 })
 
 
-    $('#email').keyup(function(){
-        if(ValidationOfEmail()){
-            $('#email_error').text('We are ready to go')
-            $('#email_error').css('color', 'blue');
-            $('.input3').css('border-bottom', '1px solid blue')
-        } else{
-            $('#email_error').text('yes we are ready to go');
-            $('#email_error').css('color', 'red');
-            $('.input3').css('border-bottom', '1px solid red')
+        //  validation of first password
+        $('#first').keyup(function(){
+        let firstpassword =$('#fist').val()
+        if(validationOfPassword()){
+            $('#password_error').text('Password Validated')
+            $('#password_error').css('color', '#00ff00');
+            $('.input5').css('border-bottom', '1px solid #00ff00')
+        }else{
+            $('#password_error').text('your password needs to morethan 8')
+            $('#password_error').css('color', '#ff0000')
+             $('.input5').css('border-bottom', '1px solid #ff0000')
         }
     })
 
-    $('#first').keyup(function(){
-        let firstpassword =$('#fist').val()
-        if(validationOfPassword()){
-            $('#password_error').text('fairly strong')
-            $('#password_error').css('color', 'blue');
-            $('.input5').css('border-bottom', '1px solid blue')
+
+    //validation of second password
+    $('#second').keyup(function(){
+        if(validationOfConfirmPass()){
+             $('#confirm_password_error').text('your passwords do not match please check them againmmm')
+            $('#confirm_password_error').css('color', '#ff0000')
+              $('.input6').css('border-bottom', '1px solid #ff0000')
         }else{
-            $('#password_error').text('your password needs to morethan 8')
-            $('#password_error').css('color', 'red')
-             $('.input5').css('border-bottom', '1px solid red')
+        if($('#first').val() === $('#second').val()){
+            $('#confirm_password_error').text('Passwords Match')
+            $('#confirm_password_error').css('color', '#00ff00')
+            $('.input6').css('border-bottom', '1px solid #00ff00')
         }
+        else{
+            $('#confirm_password_error').text('your passwords do not match please check them again')
+            $('#confirm_password_error').css('color', '#ff0000')
+              $('.input6').css('border-bottom', '1px solid #ff0000')
+        }}
     })
+
+
 
     function ValidationOfEmail(){
           let email = $('#email').val();
-          let reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w)*$/
+          let reg = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
           if (reg.test(email)){
               return true
           }else{
@@ -138,3 +150,8 @@
     }
 // }
 // 
+
+
+
+
+        //   let reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w)*$/
